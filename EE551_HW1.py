8ed658ce93d9f1eeda9c4aa9530037e6e162c36c
Grace-Tian-12 <60356438+Grace-Tian-12@users.noreply.github.com>
2020-02-10 17:24:43 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -40,18 +40,19 @@ def lists():
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    c = [[1, 4, 5],
+   import numpy as np
+    A = np.array([[1, 4, 5],
          [6, 10, 11],
-         [12, 17, 38]]
+         [12, 17, 38]])
          
     # Collect the items in the last column of matrix A using list comprehension
-
+    c = A[:,-1]
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    d = [A[i][i] for i in range(len(A))if A[i][i]%2 == 0]
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-
+    o = [ord(i) for i in 'Stevens']
     return p, r, c, d, o
 
 
@@ -63,24 +64,26 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    a = {'fruit':'apple', 'quantity':4, 'color':'green'}
     # Get the item in dictionary f that the key "fruit" maps to
-
+    f = a['fruit']
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-
+    a['quantity'] = 5
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    amazing_grace = {'name':{'first_name':'Grace', 'last_name':'Hopper'},
+         'jobs':["scientist", "engineer"],
+         'age':'85'}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    amazing_grace['jobs'].append('programmer')
     # Get the third job Grace has that you recently added
-
+    p = amazing_grace['jobs'][2]
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-
+    k = sorted(amazing_grace)
     return a, f, p, k
 
 numbers_and_strings()
